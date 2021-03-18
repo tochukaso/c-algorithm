f6274914a5e1566fd23e31ecdbc96bdd30fbf03d
Omori Yasumitsu <omori@gmo-sc.com>
2021-03-18 11:15:15 +0900

    feat: Add C++ template

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define REP(i,x) for(int i=0;i<(int)(x);i++)
+#define REPS(i,x) for(int i=1;i<=(int)(x);i++)
+#define RREP(i,x) for(int i=((int)(x)-1);i>=0;i--)
+#define RREPS(i,x) for(int i=((int)(x));i>0;i--)
+#define PERM(c) sort(all(c));for(bool c##p=1;c##p;c##p=next_permutation(all(c)))
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+
+using namespace std;
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+typedef long long ll;
+
+int gcd(int a,int b){return b?gcd(b,a%b):a;}
+const int dx[4] = { 1, 0,-1, 0};
+const int dy[4] = { 0, 1, 0,-1};
+
+int main() {
+
+  return 0;
+}
